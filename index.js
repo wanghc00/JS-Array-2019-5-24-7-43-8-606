ee6f74d55c3b10ca8968d9cdd45ec575427cb0f3
wanghc00 <306093226@qq.com>
2019-07-09 19:55:00 +0800

    2019-07-09

--- index.js
+++ index.js
@@ -4,11 +4,17 @@
 var a = '[a, b, c, d]';
 var b = [1, 2, 3, 4];
 //TODO
+a instanceof Array?alert("数组"):alert("非数组");
+b instanceof Array?alert("数组"):alert("非数组");
 
 
 // 编写程序，将下面数组中的每一项都乘以2。
 var a = [1, 2, 3, 4, 5];
 // TODO should output [2,4,6,8,10]
+a.forEach(function(item,index,array){
+    array[index]*=2;
+});
+document.write(a);
 
 
 // 编写程序，按下面的要求输出结果。
@@ -16,13 +22,30 @@ var colors = ["Red", "Green", "White", "Black"];
 //TODO case 1 output: 'Red Green White Black'
 // case 2 output: 'Red+Green+White+Black'
 // case 3 output: 'Red,Green,White,Black'
+document.write("'"+colors.join(" ")+"'");
+document.write("'"+colors.join("+")+"'");
+document.write("'"+colors.join()+"'");
 
 
 // 编写程序，将下面数组中的数字按从大到小的顺序排序。
 var a = [5, 1, 8, 10, 4];
 //TODO should output: [10,8,5,4,1]
+function sortNumber(a,b){
+	return a - b;
+}
+document.write(a.sort(sortNumber).reverse());
 
 
 // 编程程序，找出下列数组中出现频率最高的元素。
 var a = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
 //TODO should output: 'a'
+let res = {};
+let maxName, maxNum = 0;
+a.forEach(function(item){
+  res[item] ? res[item] += 1 : res[item] = 1;
+  if (res[item] > maxNum) {
+    maxName = item;
+    maxNum = res[item];
+  }
+});
+  document.write('出现次数最多的元素为:' + maxName + ', 出现次数为:' + maxNum);
